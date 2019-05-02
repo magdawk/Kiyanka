c3a7446501d0a85b54ca6bbcf9ef086dcea95509
haushinka22 <haushinka22@gmail.com>
2019-05-02 20:54:07 +0200

    Dodaj pliki projektów.

---
+++ Kiyanka/Products.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Products]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(256) NOT NULL, 
+    [Symbol] NVARCHAR(128) NOT NULL, 
+    [Price] MONEY NULL, 
+    [Tax] FLOAT NULL, 
+    [Unit] NVARCHAR(3) NULL
+)
