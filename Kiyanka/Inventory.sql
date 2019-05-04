04912198955a55b07d0ca27dcf8a29556aceb73c
haushinka22 <haushinka22@gmail.com>
2019-05-04 20:51:12 +0200

    Dodanie tabeli Inventory i Operations

---
+++ Kiyanka/Inventory.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[Product_id] INT NOT NULL PRIMARY KEY, 
+    [Amount] INT NOT NULL, 
+    [Date] DATETIME NOT NULL, 
+    CONSTRAINT [FK_Inventory_Products] FOREIGN KEY (Product_id) REFERENCES dbo.Products(Id)
+)
---
+++ Kiyanka/Operations.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Operations]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Product_id] INT NOT NULL, 
+    [Mark] NVARCHAR(1) NOT NULL, 
+    [Amount] INT NOT NULL, 
+    [Price] MONEY NOT NULL, 
+    [Date] DATETIME NOT NULL, 
+    CONSTRAINT [FK_Operations_Products] FOREIGN KEY (Product_id) REFERENCES dbo.Products(Id)
+)
