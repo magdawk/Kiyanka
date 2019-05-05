8e300a83b67ea2fd0ecf328f14b52d19bf5deec3
haushinka22 <haushinka22@gmail.com>
2019-05-05 20:52:22 +0200

    Trigger na Operations

---
+++ Kiyanka/Triggers/InsertToInventory.sql
@@ -0,0 +1,17 @@
+﻿CREATE TRIGGER InsertToInventory 
+ON dbo.Operations 
+AFTER INSERT
+AS
+
+BEGIN
+DECLARE @productid int
+DECLARE @Amountprod int
+
+SET @productid = (SELECT Product_id FROM inserted)
+
+SET @Amountprod = [dbo].[AmountProduct] (@productid)
+
+INSERT INTO dbo.Inventory (Product_id, Amount, Date) values (@productid, @Amountprod, (SELECT Date FROM inserted))
+
+END
+GO
