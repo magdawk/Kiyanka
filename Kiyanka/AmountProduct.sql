117dd512011f6ba4e690ede3d5b49a53c8f65e44
haushinka22 <haushinka22@gmail.com>
2019-05-05 13:56:05 +0200

    Funkcja sprawdzająca ilość danego produktu

---
+++ Kiyanka/AmountProduct.sql
@@ -0,0 +1,14 @@
+﻿CREATE FUNCTION [dbo].[AmountProduct] (@id int)
+RETURNS int 
+AS BEGIN
+
+DECLARE @amount int;
+
+SET @amount = (Select sum(O.Amount) 
+From Operations as O
+Where O.Mark = 'Z' and O.Product_id=@id) - (Select sum(O.Amount) 
+From Operations as O
+Where O.Mark = 'S' and O.Product_id=@id)
+
+RETURN @amount
+END
