bd9f64435aecc24853c28064ef1eaf57567d3655
haushinka22 <haushinka22@gmail.com>
2019-05-12 20:06:37 +0200

    Dodanie klasy product i productrepository

---
+++ DataAccess/Products/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Products
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public decimal Price { get; set; }
+        public decimal Tax { get; set; }
+        public string Unit { get; set; }
+    }
+}
